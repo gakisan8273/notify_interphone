da379bc00906c860c7698f54271e336296b106a8
h.itagaki <mint.daa.a2@gmail.com>
2024-03-30 13:23:18 +0900

    GPIOテスト

--- notify.py
+++ notify.py
@@ -1,1 +1,16 @@
-print('test')
+import time
+
+import RPi.GPIO as GPIO
+
+# Set up GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+# Set up GPIO pin
+input_pin = 17
+GPIO.setup(input_pin, GPIO.IN)
+
+# Main loop
+while True:
+    time.sleep(0.5)
+    if GPIO.input(input_pin):
+        print("Input received from GPIO pin {}".format(input_pin))
